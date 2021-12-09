46680ee63fb833ed9b95d9ead69e6d23e57d1ba3
Yasin Munye <cyasinsadiq@gmail.com>
2021-12-09 17:23:11 -0500

    added some css styles to the card class in index.html, added a search bar for other cities that will be searched.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var icon = document.querySelector('.icon');
-var key = 'f5a9cfc86d28ea3cb24220a9752bcf1e'
-var city = 'Atlanta'
+var time = document.querySelector('.time');
+var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
+var cityName= document.querySelector('.cityName')
+var humudity = document.querySelector('.humidity');
+var sunrise = document.querySelector('.sunrise');
+var sunset = document.querySelector('.sunset');
+var Search = document.querySelector('#Search');
+var key = 'fb9846360233e1bbeb23ed399c5b9e06';
+var city = 'Atlanta';
 var requestURL= `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`
 
 fetch(requestURL)
@@ -11,12 +19,45 @@ fetch(requestURL)
     const sunrise =  new Date(data.sys.sunrise * 1000).toLocaleTimeString();
 
     const pic = `http://openweathermap.org/img/wn/${iconId}@2x.png`
-      console.log(data);
-        icon.innerHTML= `<img src=${pic} />`
-    console.log(pic);
-    console.log(data.sys);
-}
-  
-  );
+      console.log(Search.value);
+      var newTemp = data.main.temp ;
+      var newCity = data.name;
+      newSearch = Search.value
+      var newWind = JSON.stringify(data.wind.speed)
+
+        icon.innerHTML = `<img src=${pic} />`
+        cityName.textContent = newCity;
+         temp.textContent =  newTemp;
+         wind.textContent = newWind + ' ' +'mph';
+
+    });
 
   
+  
+
+  function query() {
+    var citySearch = Search.value 
+    city = citySearch; 
+    console.log('hey');
+    
+    var requestURL= `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`
+    fetch(requestURL)
+    .then(response => response.json())
+    .then(data => {
+  
+      const iconId = JSON.stringify(data.weather[0]['icon']).replaceAll('"','')
+      const sunrise =  new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+  
+      const pic = `http://openweathermap.org/img/wn/${iconId}@2x.png`
+        console.log(Search.value);
+        var newTemp = data.main.temp ;
+        var newCity = data.name;
+        var newSearch = Search.value
+        var newWind = JSON.stringify(data.wind.speed)
+  
+          icon.innerHTML = `<img src=${pic} />`
+          cityName.textContent = newCity;
+           temp.textContent =  newTemp;
+           wind.textContent = newWind + ' ' +'mph';
+         });
+    }
